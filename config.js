52160bec42cc03c73895849f4867c9551bd47be5
doomul-collab <doomul@gmail.com>
2025-12-30 07:42:20 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const CONFIG = {
-    SUPABASE_URL: 'https://mtviblngovojyljonltq.supabase.co',
-    SUPABASE_KEY: 'sb_publishable_sJ4BTJ4wm1Nx1Mzelrj0rQ_mdDvqXLG'
+window.supabaseConfig = {
+  supabaseUrl: "https://mtviblngovojyljonltq.supabase.co",
+  supabaseKey: "sb_publishable_sJ4BTJ4wm1Nx1Mzelrj0rQ_mdDvqXLG"
 };
